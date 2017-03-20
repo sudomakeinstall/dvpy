d803decb8f729366832ede4e5ed83e6a63beab93
DVigneault <davis.vigneault@gmail.com>
2017-03-20 22:43:25 +0000

    Document complexify function

--- dvpy/complex_from_mag_and_phase.py
+++ dvpy/complex_from_mag_and_phase.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 def complex_from_mag_and_phase(m, p):
+  """
+  Given magnitude and phase, create a complex number.
+
+  Parameters
+  ----------
+  m : array-like
+    Magnitude
+  p : array-like (of same shape as m)
+    Phase
+
+  Returns
+  -------
+  c : array-like (of same shape as input)
+    Elementwise complexified array
+
+  """
   r = m * np.cos(p)
   i = m * np.sin(p)
   return r + i * 1.0j
