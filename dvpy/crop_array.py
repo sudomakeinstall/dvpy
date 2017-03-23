265180eea3bc4d0a5b9ca447f16da6b953e30e17
DVigneault <davis.vigneault@gmail.com>
2017-03-23 22:35:07 +0000

    DOC: Add documentation for test crop

--- dvpy/crop_array.py
+++ dvpy/crop_array.py
@@ -1,6 +1,23 @@
 import numpy as np
 
 def crop_array(array, origin, dim):
+  """
+  Crop an array-like given origin and dimensions
+
+  Parameters
+  ----------
+  array : array-like
+    Array to be cropped.
+  origin : tuple
+    Indices of starting point to be cropped
+  dim : tuple
+    Dimensions to crop array to
+
+  Returns
+  -------
+  out : array-like
+    Cropped array
+  """
 
   start = np.array(origin)
   end = start + np.array(dim)
--- test/test_crop_image.py
+++ test/test_crop_image.py
@@ -5,3 +5,7 @@ def test_crop_array():
   im = np.zeros((10, 10, 10))
   cr = crop_array(im, (2, 2, 2), (8, 8, 8))
   assert(cr.shape == (8, 8, 8))
+
+  im = np.matrix(np.zeros((10, 10)))
+  cr = crop_array(im, (2, 2), (8, 8))
+  assert(cr.shape == (8, 8))
