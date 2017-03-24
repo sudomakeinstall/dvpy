821b4bb0de05d33afef2fb23e89039889e0ec0f2
DVigneault <davis.vigneault@gmail.com>
2017-03-24 10:30:55 +0000

    ENH: Add wrap phase test

--- dvpy/wrap_phase.py
+++ dvpy/wrap_phase.py
@@ -1,5 +1,19 @@
 import numpy as np
 
-def wrap_phase(image):
-  """ Wrap the input into the range [-pi, pi]."""
-  return (image + np.pi) % (2.0 * np.pi) - np.pi
+def wrap_phase(x):
+  """
+  Wrap the input into the range [-pi, pi].
+
+  Parameters
+  ----------
+  x: array-like
+    Number, array, or matrix to be phase wrapped.
+
+  Returns
+  -------
+  output: array-like
+    A structure of the same shape as the input wrapped
+    into the range [-pi, pi] elementwise.
+
+  """
+  return (x + np.pi) % (2.0 * np.pi) - np.pi
