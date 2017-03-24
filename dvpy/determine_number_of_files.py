c1a044fecb2b3f98a997b98e5138e8a3412a60ca
DVigneault <davis.vigneault@gmail.com>
2017-03-24 10:02:50 +0000

    ENH: Change interface for determine_number_of_files, add tests

--- dvpy/determine_number_of_files.py
+++ dvpy/determine_number_of_files.py
@@ -1,7 +1,25 @@
 import os
 
-def determine_number_of_files(directory, extension):
+def determine_number_of_files(directory, pattern):
+  """
+  Determine how many files in a directory match a given pattern
+  after substituting sequential integers starting from zero.
+
+  Parameters
+  ----------
+  directory: str
+    Base directory in which to look for files.
+  pattern: str
+    String pattern allowing a single digit substitution
+    e.g., "img_%d.png"%(0) ==> "img_0.png"
+
+  Returns
+    N : int
+    Number of files matching pattern sequentially, beginning with zero.
+  -------
+  """
+
   number_of_files = 0
-  while (os.path.isfile(os.path.join(directory, '%d.%s'%(number_of_files, extension)))):
+  while (os.path.isfile(os.path.join(directory, pattern%(number_of_files)))):
     number_of_files += 1
   return number_of_files
