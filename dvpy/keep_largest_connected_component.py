1ea69cd7f533aacb85ebd2b50655a9f21ca39581
DVigneault <davis.vigneault@gmail.com>
2017-05-08 09:09:30 +0100

    BUG: Deal with empty image special case.

--- dvpy/keep_largest_connected_component.py
+++ dvpy/keep_largest_connected_component.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage.morphology import label
 
 def keep_largest_connected_component(image):
+  if np.sum(image) == 0: return image
   labels, num = label(image, return_num = True)
   sums = [np.sum(labels == n) for n in range(1, num+1)]
   label_to_keep = np.argmax(sums) + 1
