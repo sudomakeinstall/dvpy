4900ea2e21303995fb54f710c9b49c322566e758
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-08 15:09:13 +0100

    Missing import

--- dvpy/wrapped_phase_difference.py
+++ dvpy/wrapped_phase_difference.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .wrap_phase import wrap_phase
 
 def wrapped_phase_difference(x, axis):
-  return np.minimum(np.diff(x, 1, axis), np.diff(dv.wrap_phase(x + np.pi), 1, axis))
+  return np.minimum(np.diff(x, 1, axis), np.diff(wrap_phase(x + np.pi), 1, axis))
