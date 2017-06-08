fc754e8c3973a85907782179b5a9168cd07b41ef
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-08 15:23:53 +0100

    Change implementation

--- dvpy/wrapped_phase_difference.py
+++ dvpy/wrapped_phase_difference.py
@@ -2,4 +2,9 @@ import numpy as np
 from .wrap_phase import wrap_phase
 
 def wrapped_phase_difference(x, axis):
-  return np.minimum(np.diff(x, 1, axis), np.diff(wrap_phase(x + np.pi), 1, axis))
+  x1 = np.diff(x, 1, axis)
+  x2 = np.diff(wrap_phase(x + np.pi), 1, axis)
+  out = x1
+  out[np.real(x) >= 0] = x2[np.real(x) >= 0]
+
+  return out
