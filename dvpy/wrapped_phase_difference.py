78b26547e01d6916669e41d5bbdbd7186971c162
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-16 11:54:59 +0100

    Unwrap first

--- dvpy/wrapped_phase_difference.py
+++ dvpy/wrapped_phase_difference.py
@@ -1,9 +1,11 @@
 import numpy as np
+import unwrap as uw
 from .wrap_phase import wrap_phase
 
 def wrapped_phase_difference(x, ax):
 #  x1 = np.diff(x, 1, axis)
 #  x2 = np.diff(wrap_phase(x + np.pi), 1, axis)
+  x = uw.unwrap(x)
   x1 = np.gradient(x,                     axis = ax)
   x2 = np.gradient(wrap_phase(x + np.pi), axis = ax)
   out = x1
