ee015b72a6dfa8843edb84d5950a70dfb462b9aa
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-16 15:31:41 +0100

    Move to src

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='dvpy',
-      version='0.1',
+      version='0.1.0',
       description='Python Utilities',
       author='Davis Marc Vigneault',
       author_email='davis.vigneault@gmail.com',
       url='https://www.github.com/DVigneault/',
-      packages=['dvpy'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
      )
