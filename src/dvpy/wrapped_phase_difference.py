dcf64e8dbea41478769de8982c8317339ed81e72
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-16 16:56:36 +0100

    BUG: Fix padding issue.

--- src/dvpy/wrapped_phase_difference.py
+++ src/dvpy/wrapped_phase_difference.py
@@ -1,14 +1,9 @@
 import numpy as np
-import unwrap as uw
 from .wrap_phase import wrap_phase
 
 def wrapped_phase_difference(x, ax):
-#  x1 = np.diff(x, 1, axis)
-#  x2 = np.diff(wrap_phase(x + np.pi), 1, axis)
-  x = uw.unwrap(x)
-  x1 = np.gradient(x,                     axis = ax)
-  x2 = np.gradient(wrap_phase(x + np.pi), axis = ax)
-  out = x1
-  out[np.real(x) >= 0] = x2[np.real(x) >= 0]
-
-  return out
+  padding = [[0, 0] for _ in range(2)]
+  padding[ax][1] = 1
+  x1 = np.pad(np.diff(x,                     n = 1, axis = ax), padding, 'edge')
+  x2 = np.pad(np.diff(wrap_phase(x + np.pi), n = 1, axis = ax), padding, 'edge')
+  return np.minimum(x1, x2)
