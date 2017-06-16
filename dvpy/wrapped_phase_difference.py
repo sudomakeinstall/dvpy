f0a70f82abc494ac4547c4abed939c06d3df7365
Davis Vigneault <davis.vigneault@gmail.com>
2017-06-16 11:41:26 +0100

    Use Gradient rather than Diff

--- dvpy/wrapped_phase_difference.py
+++ dvpy/wrapped_phase_difference.py
@@ -1,9 +1,11 @@
 import numpy as np
 from .wrap_phase import wrap_phase
 
-def wrapped_phase_difference(x, axis):
-  x1 = np.diff(x, 1, axis)
-  x2 = np.diff(wrap_phase(x + np.pi), 1, axis)
+def wrapped_phase_difference(x, ax):
+#  x1 = np.diff(x, 1, axis)
+#  x2 = np.diff(wrap_phase(x + np.pi), 1, axis)
+  x1 = np.gradient(x,                     axis = ax)
+  x2 = np.gradient(wrap_phase(x + np.pi), axis = ax)
   out = x1
   out[np.real(x) >= 0] = x2[np.real(x) >= 0]
 
