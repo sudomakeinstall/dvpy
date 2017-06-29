1100951ad3fa376df8afb5960c5c2109c1868ff9
DVigneault <davis.vigneault@gmail.com>
2017-06-29 15:29:24 +0100

    BUG: Wrapped phase difference should work for pos and neg transitions.

--- src/dvpy/wrapped_phase_difference.py
+++ src/dvpy/wrapped_phase_difference.py
@@ -1,9 +1,11 @@
 import numpy as np
-from .wrap_phase import wrap_phase
+import dvpy as dv
 
 def wrapped_phase_difference(x, ax):
   padding = [[0, 0] for _ in range(2)]
   padding[ax][1] = 1
   x1 = np.pad(np.diff(x,                     n = 1, axis = ax), padding, 'edge')
-  x2 = np.pad(np.diff(wrap_phase(x + np.pi), n = 1, axis = ax), padding, 'edge')
-  return np.minimum(x1, x2)
+  x2 = np.pad(np.diff(dv.wrap_phase(x + np.pi), n = 1, axis = ax), padding, 'edge')
+  out = x1
+  out[np.absolute(x1) > np.absolute(x2)] = x2[np.absolute(x1) > np.absolute(x2)]
+  return out
