369a7fee0e223573c3b88258148fb3f6f3f53e5a
DVigneault <davis.vigneault@gmail.com>
2017-08-18 19:08:20 -0700

    Return non-binarized image

--- src/dvpy/mask_to_transform.py
+++ src/dvpy/mask_to_transform.py
@@ -12,6 +12,7 @@ def mask_to_transform(seg, rotation = 0.0, margin = 0.0, radians = True, return_
 
   assert(seg.ndim == 2)
 
+  seg_original = seg.copy()
   seg = dv.binarize_array(seg)
 
   ##
@@ -30,12 +31,7 @@ def mask_to_transform(seg, rotation = 0.0, margin = 0.0, radians = True, return_
   ## Translation
   ##
 
-  seg_xy = itp.shift(seg,
-             ost,
-             order = 0,
-             mode = 'constant',
-             cval = 0,
-            )
+  seg_xy = itp.shift(seg, ost, order = 0, mode = 'constant', cval = 0,)
 
   ##
   ## Rotation
@@ -55,9 +51,11 @@ def mask_to_transform(seg, rotation = 0.0, margin = 0.0, radians = True, return_
 
   scale = min(1.0, scale + margin*2.0)
 
-  seg_zm = dv.crop_or_pad(itp.zoom(seg_rt, 1.0 / scale, order = 0), seg.shape)
 
   if return_images:
+    seg_xy = itp.shift(seg_original, ost, order = 0, mode = 'constant', cval = 0,)
+    seg_rt = itp.rotate(seg_xy, rotation, reshape = False, order = 0, mode = 'nearest')
+    seg_zm = dv.crop_or_pad(itp.zoom(seg_rt, 1.0 / scale, order = 0), seg.shape)
     return nrm, -np.radians(rotation), scale, seg_xy, seg_rt, seg_zm
   else:
     return nrm, -np.radians(rotation), scale
