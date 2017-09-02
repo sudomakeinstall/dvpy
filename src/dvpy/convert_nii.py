2a468c183bf10329112952d980f62d491a048696
DVigneault <davis.vigneault@gmail.com>
2017-09-02 13:50:49 -0700

    Convert and rescale Nifti

---
+++ src/dvpy/convert_nii.py
@@ -0,0 +1,14 @@
+import nibabel as nb
+import numpy as np
+import dvpy as dv
+import skimage.exposure as ex
+
+def convert_nii(im, new_dtype = 'uint16'):
+  d, a, h = dv.copy_nii(im, return_components = True)
+  d = np.nan_to_num(d)
+  d = ex.rescale_intensity(d, in_range = 'image', out_range = new_dtype)
+
+  new_image = nb.Nifti1Image(d, a, header = h)
+  new_image.set_data_dtype(new_dtype)
+
+  return new_image
---
+++ test/test_convert_nii.py
@@ -0,0 +1,9 @@
+import dvpy as dv
+import nibabel as nb
+import numpy as np
+
+def test_convert_nii():
+  data = np.random.random_integers(low = 0, high = 100, size = (5, 5, 5))
+  im = nb.Nifti1Image(data, np.eye(4))
+  for dtype in ['uint8', 'int8', 'uint16', 'int16']:
+    dv.convert_nii(im, new_dtype = dtype)
