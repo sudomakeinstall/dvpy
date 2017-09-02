3491948b435dea3b29c11dbe191fc45282af495c
DVigneault <davis.vigneault@gmail.com>
2017-09-02 15:20:05 -0700

    Rescaling should be optional

--- src/dvpy/convert_nii.py
+++ src/dvpy/convert_nii.py
@@ -3,10 +3,13 @@ import numpy as np
 import dvpy as dv
 import skimage.exposure as ex
 
-def convert_nii(im, new_dtype):
+def convert_nii(im, new_dtype, rescale = False):
   d, a, h = dv.copy_nii(im, return_components = True)
-  d = np.nan_to_num(d)
-  d = ex.rescale_intensity(d, in_range = 'image', out_range = new_dtype)
+  if rescale:
+    d = np.nan_to_num(d)
+    d = ex.rescale_intensity(d, in_range = 'image', out_range = new_dtype)
+  else:
+    d = d.astype(new_dtype)
 
   new_image = nb.Nifti1Image(d, a, header = h)
   new_image.set_data_dtype(new_dtype)
--- test/test_convert_nii.py
+++ test/test_convert_nii.py
@@ -5,5 +5,6 @@ import numpy as np
 def test_convert_nii():
   data = np.random.random_integers(low = 0, high = 100, size = (5, 5, 5))
   im = nb.Nifti1Image(data, np.eye(4))
-  for dtype in ['uint8', 'int8', 'uint16', 'int16']:
-    dv.convert_nii(im, new_dtype = dtype)
+  for rescale in [True, False]:
+    for dtype in ['uint8', 'int8', 'uint16', 'int16']:
+      dv.convert_nii(im, dtype, rescale = rescale)
