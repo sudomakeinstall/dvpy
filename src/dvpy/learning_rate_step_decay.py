e29c4ba11301dd855d520161e2a49033f46fb6d1
Davis Vigneault <davis.vigneault@gmail.com>
2018-08-08 06:52:01 -0700

    Blacken learning_rate_ste_decay

--- src/dvpy/learning_rate_step_decay.py
+++ src/dvpy/learning_rate_step_decay.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def learning_rate_step_decay(epoch, lr, step=24, initial_power=-3):
     """
     The learning rate begins at 10^initial_power,
@@ -9,4 +10,3 @@ def learning_rate_step_decay(epoch, lr, step=24, initial_power=-3):
     lrate = 10 ** (initial_power - num)
     print("Learning rate for epoch {} is {}.".format(epoch + 1, 1.0 * lrate))
     return np.float(lrate)
-
