a3ce5cfcaf80d90c017039551cd42389a3ec3633
nitesh1813 <niteshsekhar@gmail.com>
2018-08-08 18:10:07 -0700

    Nitesh added padding

--- src/dvpy/tf/unet.py
+++ src/dvpy/tf/unet.py
@@ -5,6 +5,7 @@ from keras.regularizers import l2
 from keras.initializers import Orthogonal
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv3D, MaxPooling3D,Input,UpSampling3D,Reshape,ZeroPadding3D
 from keras.layers import (
     Input,
     Conv1D,
@@ -17,6 +18,9 @@ from keras.layers import (
     UpSampling2D,
     UpSampling3D,
     Reshape,
+    ZeroPadding1D,
+    ZeroPadding2D,
+    ZeroPadding3D,
 )
 from keras.layers.merge import concatenate, multiply
 from keras.engine import Layer
@@ -29,7 +33,7 @@ import tensorflow as tf
 conv_dict = {1: Conv1D, 2: Conv2D, 3: Conv3D}
 max_pooling_dict = {1: MaxPooling1D, 2: MaxPooling2D, 3: MaxPooling3D}
 up_sampling_dict = {1: UpSampling1D, 2: UpSampling2D, 3: UpSampling3D}
-
+zero_sampling_dict = {1: ZeroPadding1D, 2: ZeroPadding2D, 3: ZeroPadding3D}
 
 def conv_bn_relu_1x(nb_filter, kernel_size, subsample, dimension, weight_decay):
     sub = subsample * dimension
@@ -67,7 +71,15 @@ def conv_bn_relu(
 
     return f
 
-
+def get_padding(shape,dimension):
+    x_crop = 1 if shape[1]%2 ==1 else 0
+    if (dimension ==1):
+        return ((0,x_crop))
+    y_crop = 1 if shape[2]%2 ==1 else 0
+    if(dimension == 2):
+        return ((0,x_crop),(0,y_crop))
+    z_crop = 1 if shape[3]%2 ==1 else 0
+    return ((0,x_crop),(0,y_crop),(0,z_crop))
 def get_unet(
     dim,
     num_output_classes,
@@ -93,6 +105,7 @@ def get_unet(
 
     MaxPooling = max_pooling_dict[dimension]
     UpSampling = up_sampling_dict[dimension]
+    ZeroPadding = zero_sampling_dict[dimension]
     Conv = conv_dict[dimension]
 
     def f(input_layer):
@@ -129,9 +142,10 @@ def get_unet(
                         conv_depth[i + unet_depth], kernel_size, dimension=dimension
                     )(us[-1])
                 ]
+            padding = get_padding(K.int_shape(levels[unet_depth - i - 1]),dimension)
             us += [
                 concatenate(
-                    [UpSampling(size=pool_size)(levels[-1]), levels[unet_depth - i - 1]]
+                    [ZeroPadding(padding)(UpSampling(size=pool_size)(levels[-1])), levels[unet_depth - i - 1]]
                 )
             ]
 
@@ -147,6 +161,7 @@ def get_unet(
             kernel_regularizer=l2(weight_decay),
             bias_regularizer=l2(weight_decay),
         )(final_feature)
+        # print(conv11.shape)
         conv12 = Reshape((np.product(dim), num_output_classes))(conv11)
         conv13 = Activation(activation="softmax")(conv12)
 
