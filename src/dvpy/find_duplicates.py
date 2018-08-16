766ad9286585d28bbf46278bba7bcbc99c2aba5e
Davis Vigneault <davis.vigneault@gmail.com>
2018-08-16 10:08:28 -0700

    Blacken

--- src/dvpy/find_duplicates.py
+++ src/dvpy/find_duplicates.py
@@ -1,3 +1,2 @@
-
 def find_duplicates(l):
-  return set([_l for _l in l if l.count(_l) > 1])
+    return set([_l for _l in l if l.count(_l) > 1])
--- test/test_find_duplicates.py
+++ test/test_find_duplicates.py
@@ -1,7 +1,8 @@
 
 import dvpy as dv
 
+
 def test_find_duplicates():
 
-    dups = dv.find_duplicates(['a', 'a', 'b', 'c', 'c'])
-    assert((dups - {'a', 'c'}) == set([]))
+    dups = dv.find_duplicates(["a", "a", "b", "c", "c"])
+    assert (dups - {"a", "c"}) == set([])
