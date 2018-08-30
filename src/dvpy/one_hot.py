69ab1c1bdc708a6d7d403bd886064471a11ceae6
Davis Vigneault <davis.vigneault@gmail.com>
2018-08-29 21:24:54 -0400

    Add documentation to one_hot

--- src/dvpy/one_hot.py
+++ src/dvpy/one_hot.py
@@ -7,11 +7,20 @@ import keras.utils.np_utils as k
 # Internal
 
 
-def one_hot(image, num_classes):
+def one_hot(array, num_classes):
     """
-  One-hot encode an image
-  """
+    One-hot encode an array.
+
+    :param array: An input segmentation.
+    :type array: nd-array with unsigned integers.
+
+    :param num_classes: The length of the output vector.
+    :type num_classes: unsigned int
+
+    :returns: The segmentation, with each pixel coded as a one-hot array.
+    :rtype: array-like, of shape array.shape + (num_classes,)
+    """
     return np.reshape(
-        k.to_categorical(image.flatten(), num_classes=num_classes),
-        image.shape + (num_classes,),
+        k.to_categorical(array.flatten(), num_classes=num_classes),
+        array.shape + (num_classes,),
     )
