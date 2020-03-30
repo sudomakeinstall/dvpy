27bdd6291342469b82fb0ba6873b0b64c66087e9
Davis Marc Vigneault <davis.vigneault@gmail.com>
2020-03-30 18:54:23 -0400

    Fix failing test

--- test/test_ImageDataGenerator.py
+++ test/test_ImageDataGenerator.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def test_ImageDataGenerator():
 
-    gen = dv.tf.ImageDataGenerator(3)
+    gen = dv.tf.ImageDataGenerator(3, ["input_image"], ["unet_seg"])
     gen.random_transform(np.ones((25, 25, 25, 3)), np.ones((25, 25, 25, 3)))
