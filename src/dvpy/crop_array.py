6883a353fc7192efc7db719cbabc60e6f16efa85
Davis Marc Vigneault <davis.vigneault@gmail.com>
2020-03-30 23:00:38 -0400

    Fix warning due to indexing

--- src/dvpy/crop_array.py
+++ src/dvpy/crop_array.py
@@ -8,22 +8,22 @@ import numpy as np
 
 def crop_array(array, origin, dim):
     """
-  Crop an array-like given origin and dimensions
+    Crop an array-like given origin and dimensions
 
-  :param array: Array to be cropped.
-  :type array: array-like
+    :param array: Array to be cropped.
+    :type array: array-like
 
-  :param origin: Indices of starting point to be cropped
-  :type origin: tuple
+    :param origin: Indices of starting point to be cropped
+    :type origin: tuple
 
-  :param dim: Dimensions to crop array to
-  :type dim: tuple
+    :param dim: Dimensions to crop array to
+    :type dim: tuple
 
-  :returns: Cropped array
-  :rtype: array-like
-  """
+    :returns: Cropped array
+    :rtype: array-like
+    """
 
     start = np.array(origin)
     end = start + np.array(dim)
     slices = [slice(s, e) for (s, e) in zip(start, end)]
-    return array[slices]
+    return array[tuple(slices)]
--- test/test_crop_array.py
+++ test/test_crop_array.py
@@ -1,12 +1,8 @@
-from dvpy import crop_array
+import dvpy as dv
 import numpy as np
 
 
 def test_crop_array():
     im = np.zeros((10, 10, 10))
-    cr = crop_array(im, (2, 2, 2), (8, 8, 8))
-    assert cr.shape == (8, 8, 8)
-
-    im = np.matrix(np.zeros((10, 10)))
-    cr = crop_array(im, (2, 2), (8, 8))
-    assert cr.shape == (8, 8)
+    cr = dv.crop_array(im, (2, 2, 2), (8, 8, 8))
+    assert(cr.shape == (8, 8, 8))
