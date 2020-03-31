3a8813e7fe42e65d328c3c1259917927bac8b4d1
Davis Marc Vigneault <davis.vigneault@gmail.com>
2020-03-31 17:09:49 -0400

    Avoid deprecated numpy function

--- test/test_convert_nii.py
+++ test/test_convert_nii.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def test_convert_nii():
-    data = np.random.random_integers(low=0, high=100, size=(5, 5, 5))
+    data = np.random.randint(low=0, high=101, size=(5, 5, 5))
     im = nb.Nifti1Image(data, np.eye(4))
     for rescale in [True, False]:
         for dtype in ["uint8", "int8", "uint16", "int16"]:
